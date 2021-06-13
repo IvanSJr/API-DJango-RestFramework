5e58f0dd1a947d1da1d7819612183dbe9f1246fd
IvanSJr <ivan.junior2706@gmail.com>
2021-06-13 20:41:07 -0300

    Configurando views e endpoints para criar o metodo GET.

--- cursos/views.py
+++ cursos/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Curso, Avaliacao
+from .serializers import CursoSerializer, AvaliacaoSerializer
+
+
+class CursoApiView(APIView):
+    """
+    Api de Cursos
+    """
+
+    def get(self, request):
+        cursos = Curso.objects.all()
+        serializer = CursoSerializer(cursos, many=True)
+        return Response(serializer.data)
+
+
+class AvaliacaoApiView(APIView):
+    """
+    Api de Avaliações
+    """
+
+    def get(self, request):
+        avaliacoes = Avaliacao.objects.all()
+        serializer = AvaliacaoSerializer(avaliacoes, many=True)
+        return Response(serializer.data)
+
