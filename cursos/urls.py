6afe363e4df8921b10f9151875bfc21c22fe93e5
IvanSJr <ivan.junior2706@gmail.com>
2021-06-14 19:43:37 -0300

    Criando classes para Update, Delete, Patch e configurando seus endpoints.

--- cursos/urls.py
+++ cursos/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import CursoApiView, AvaliacaoApiView
+from .views import CursosApiView, AvaliacoesApiView, CursoApiView, AvaliacaoApiView
 
 urlpatterns = [
-    path('cursos/', CursoApiView.as_view(), name='cursos'),
-    path('avaliacoes/', AvaliacaoApiView.as_view(), name='avaliacoes')
+    path('cursos/', CursosApiView.as_view(), name='cursos'),
+    path('avaliacoes/', AvaliacoesApiView.as_view(), name='avaliacoes'),
+    path('cursos/<int:pk>/', CursoApiView.as_view(), name='curso'),
+    path('avaliacoes/<int:pk>/', AvaliacaoApiView.as_view(), name='avaliacao')
 ]
--- cursos/views.py
+++ cursos/views.py
@@ -4,11 +4,21 @@ from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
 
 
-class CursoApiView(generics.ListCreateAPIView):
+class CursosApiView(generics.ListCreateAPIView):
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
 
 
-class AvaliacaoApiView(generics.ListCreateAPIView):
+class CursoApiView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+
+
+class AvaliacoesApiView(generics.ListCreateAPIView):
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer
+
+
+class AvaliacaoApiView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
